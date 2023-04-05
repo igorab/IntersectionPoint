dfc9772ef7453df780d448c5a2dcf61836eef5b7
Igor <igor.balakirev@mail.ru>
2023-04-05 19:25:26 +0300

    IntersectionType::E

---
+++ IntersectionObjects.cpp
@@ -0,0 +1,222 @@
+// Написать код на С++для определения точки пересечения отрезка и
+// треугольника в 3D пространстве.Отрезок задан координатами концов,
+// треугольник задан координатами всех трех углов.
+
+#include <iostream>
+
+using namespace std;
+typedef float num;
+const int DIM = 3;
+const float EPSILON = 0.0001;
+
+struct IShape
+{
+	virtual num len() = 0;
+	virtual num area() = 0;
+};
+
+// точка в пространстве
+struct Point : IShape
+{
+	num X = 0;
+	num Y = 0;
+	num Z = 0;
+
+	explicit Point(num _X = 0, num _Y = 0, num _Z = 0) : X(_X), Y(_Y), Z(_Z) {}
+
+	friend ostream& operator<< (ostream& out, const Point& point);
+
+	friend istream& operator>> (istream& in, Point& point);
+
+	num* getTPoint()
+	{
+		static num Tp[DIM] = { X, Y, Z };
+		return Tp;
+	}
+
+	num len() { return 0; }
+	num area() { return 0; }
+};
+
+// вектор
+struct Vector
+{
+	num X, Y, Z;
+
+	Vector() : X(0), Y(0), Z(0) {}
+	Vector(num _X, num _Y, num _Z) : X(_X), Y(_Y), Z(_Z) {}
+	Vector(Point _point) {
+		X = _point.X;
+		Y = _point.Y;
+		Z = _point.Z;
+	}
+	Vector(Point _from, Point _to) {
+		X = _to.X - _from.X;
+		Y = _to.Y - _from.Y;
+		Z = _to.Z - _from.Z;
+	}
+
+	Point getPoint() { return Point(X, Y, Z); }
+
+	// модуль вектора
+	num len() const {
+		num sumV = X * X + Y * Y + Z * Z;
+		return sqrt(sumV);
+	}
+
+	// произведение вектора на число
+	Vector mult3(const num C)
+	{
+		Vector vc(C * X, C * Y, C * Z);
+		return vc;
+	}
+
+	//сложение векторов
+	void sum3(const Vector _V, int sign)
+	{
+		X += sign * _V.X;
+		Y += sign * _V.Y;
+		Z += sign * _V.Z;
+	}
+
+	num dot3(const Vector _b)
+	{
+		return X * _b.X + Y * _b.Y + Z * _b.Z;
+	}
+
+	//векторное произведение
+	Vector cross3(const Vector _V)
+	{
+		Vector vNorm(Y * _V.Z - Z * _V.Y,
+			Z * _V.X - X * _V.Z,
+			X * _V.Y - Y * _V.X);
+		return vNorm;
+	}
+
+	num det3(Vector a, Vector _V)
+	{
+		num det;
+		det = a.X * (Y * _V.Z - Z * _V.Y) +
+			a.Y * (Z * _V.X - X * _V.Z) +
+			a.Z * (X * _V.Y - Y * _V.X);
+		return det;
+	}
+
+	friend Vector operator+ (Vector const _A, Vector const _B) { return Vector(_A.X + _B.X, _A.Y + _B.Y, _A.Z + _B.Z); }
+	friend Vector operator- (Vector const _A, Vector const _B) { return Vector(_A.X - _B.X, _A.Y - _B.Y, _A.Z - _B.Z); }
+	// скалярное произведение
+	friend num operator*(Vector const _a, Vector const _b) { return _a.X * _b.X + _a.Y * _b.Y + _a.Z * _b.Z; }
+	// умножение на число
+	friend Vector operator*(num const _c, Vector const _V) {
+		Vector V(_V);
+		Vector c_V = V.mult3(_c);
+		return c_V;
+	}
+	//Векторное произведение
+	friend Vector operator^(Vector const _a, Vector const _b) {
+		Vector A(_a);
+		Vector normV = A.cross3(_b);
+		return normV;
+	}
+};
+
+// Отрезок
+class Segment :IShape
+{
+private:
+	Point L_A;
+	Point L_B;
+public:
+
+	Point getA() const { return L_A; };
+	Point getB() const { return L_B; };
+
+	Vector Q;
+	Vector R;
+
+	// направляющий вектор
+	Vector dir;
+
+	explicit Segment(Point _A, Point _B) : L_A(_A), L_B(_B) {
+		dir = Vector(L_A, L_B);
+		Q = Vector(L_A);
+		R = Vector(L_B);
+	}
+
+	num len() { return dir.len(); }
+	num area() { return 0; }
+};
+
+
+//    треугольник
+class Triangle : IShape
+{
+private:
+	Point P_A;
+	Point P_B;
+	Point P_C;
+
+public:
+
+	Vector V_AB;
+	Vector V_AC;
+	Vector V_BC;
+
+	explicit Triangle(Point _A, Point _B, Point _C) : P_A(_A), P_B(_B), P_C(_C)
+	{
+		V_AB = Vector(P_A, P_B);
+		V_AC = Vector(P_A, P_C);
+		V_BC = Vector(P_B, P_C);
+	}
+
+	Point getA() const { return P_A; };
+	Point getB() const { return P_B; };
+	Point getC() const { return P_C; };
+
+	// описать уравнение плоскости через 3 точки
+	// 
+	//  вектор нормали
+	Vector norm()
+	{
+		Vector V_N = V_AB ^ V_AC;
+		return V_N;
+	}
+
+	friend ostream& operator<<(ostream& out, Triangle& _T) {
+		out << _T.P_A << " " << _T.P_B << " " << _T.P_C << endl;
+		return out;
+	}
+
+	Triangle Triangle_Proj2D(int _O_XYZ)
+	{
+		Point _A, _B, _C;
+		Triangle tr(_A, _B, _C);
+
+		if (_O_XYZ == 0)
+		{
+			_A = Point(P_A.Y, P_A.Z, 0);
+			_B = Point(P_B.Y, P_B.Z, 0);
+			_C = Point(P_C.Y, P_C.Z, 0);
+			tr = Triangle(_A, _B, _C);
+		}
+		else if (_O_XYZ == 1)
+		{
+			_A = Point(P_A.X, P_A.Z, 0);
+			_B = Point(P_B.X, P_B.Z, 0);
+			_C = Point(P_C.X, P_C.Z, 0);
+			tr = Triangle(_A, _B, _C);
+		}
+		else if (_O_XYZ == 2)
+		{
+			_A = Point(P_A.X, P_A.Y, 0);
+			_B = Point(P_B.X, P_B.Y, 0);
+			_C = Point(P_C.X, P_C.Y, 0);
+			tr = Triangle(_A, _B, _C);
+		}
+
+		return tr;
+	}
+
+	num len() { return V_AB.len() + V_AC.len() + V_BC.len(); }
+	num area() { return (V_AB ^ V_AC).len() / 2; }
+};
--- IntersectionPoint.cpp
+++ IntersectionPoint.cpp
@@ -1,263 +1,56 @@
-// Написать код на С++для определения точки пересечения отрезка и
-// треугольника в 3D пространстве.Отрезок задан координатами концов,
-// треугольник задан координатами всех трех углов.
+#pragma once
+#include "IntersectionObjects.cpp"
 
-#include <iostream>
-
-using namespace std;
-typedef float num;
-const int DIM = 3;
-const float EPSILON = 0.0000001;
-
-typedef int tPointi[DIM];
-typedef double tPointd[DIM];
-
-
-enum class IntersectionType{
+enum class IntersectionType {	
+	/// Check plane: 	
 	p, //The segment lies wholly within the plane.  
 	q, //The (first) q endpoint is on the plane (but not 'p'). 
 	r, //The (second) r endpoint is on the plane (but not 'p'). 
 	p0, //The segment lies strictly to one side or the other of the plane. 
 	p1,  //The segment intersects the plane, and none of {p, q, r} hold. 
-	////
-	f,
-	v,
-	e,
-	f0,
-	///
-	V,
-	E,
-	F,
+	// Check triangle:
+	v, // The open segment includes a vertex of T. 
+	e, // The open segment includes a point in the relative interior of an edge of T 
+	f, // The open segment includes a point in the relative interior of a face of T. 
+	f0, 
+	// Check triangle:
+	V, //p coincides with a Vertex of T. 
+	E, //p is in the relative interior of an Edge of T.
+	F, //p is in the relative interior of a Face of T.
 	T0,
 	///
 	in_plane
 };
 
+const char* txt_result[] = {"The segment lies wholly within the plane", 
+					 "The (first) q endpoint is on the plane (but not 'p')",   
+					 "The(second) r endpoint is on the plane(but not 'p')",
+				     "The segment lies strictly to one side or the other of the plane",
+					 "The segment intersects the plane, and none of {p, q, r} hold",
+					 "The open segment includes a vertex of triangle",
+					 "The open segment includes a point in the relative interior of an edge of triangle",
+					 "The open segment includes a point in the relative interior of a face of triangle",
+					 "",
+					 "Point coincides with a Vertex of triangle",
+					 "Point is in the relative interior of an Edge of triangle",
+					 "Point is in the relative interior of a Face of triangle",
+					 "",
+					 ""};
 
-struct IShape
-{
-	virtual num len() = 0;
-	virtual num area() = 0;
-};
-
-// точка в пространстве
-struct Point : IShape
-{
-	num X = 0;
-	num Y = 0;
-	num Z = 0;
-	
-	explicit Point(num _X = 0, num _Y = 0, num _Z = 0) : X(_X), Y(_Y), Z(_Z) {}
-
-	friend ostream& operator<< (ostream& out, const Point& point);
-
-	friend istream& operator>> (istream& in, Point& point);
-
-	num* getTPoint()
-	{
-		static num Tp[DIM] = { X, Y, Z };
-		return Tp;
-	}
-
-	num len() { return 0; }
-	num area() { return 0; }
-};
-
-// вектор
-struct Vector
-{
-	num X, Y, Z;
-
-	Vector() : X(0), Y(0), Z(0) {}
-	Vector(num _X, num _Y, num _Z) : X(_X), Y(_Y), Z(_Z) {}
-	Vector(Point _point) {
-		X = _point.X;
-		Y = _point.Y;
-		Z = _point.Z;
-	}
-	Vector(Point _from, Point _to) {
-		X = _to.X - _from.X;
-		Y = _to.Y - _from.Y;
-		Z = _to.Z - _from.Z;
-	}
-
-	Point getPoint() { return Point(X, Y, Z); }
-
-	// модуль вектора
-	num len() const {
-		num sumV = X * X + Y * Y + Z * Z;
-		return sqrt(sumV);
-	}
-
-	// произведение вектора на число
-	Vector mult3(const num C)
-	{
-		Vector vc(C * X, C * Y, C * Z);
-		return vc;
-	}
-
-	//сложение векторов
-	void sum3(const Vector _V, int sign)
-	{
-		X += sign * _V.X;
-		Y += sign * _V.Y;
-		Z += sign * _V.Z;
-	}
-
-	num dot3(const Vector _b)
-	{
-		return X * _b.X + Y * _b.Y + Z * _b.Z;
-	}
-
-	//векторное произведение
-	Vector cross3(const Vector _V)
-	{
-		Vector vNorm(Y * _V.Z - Z * _V.Y,
-			Z * _V.X - X * _V.Z,
-			X * _V.Y - Y * _V.X);
-		return vNorm;
-	}
-
-	num det3(Vector a, Vector _V)
-	{
-		num det;
-		det = a.X * (Y * _V.Z - Z * _V.Y) +
-			a.Y * (Z * _V.X - X * _V.Z) +
-			a.Z * (X * _V.Y - Y * _V.X);
-		return det;
-	}
-
-	friend Vector operator+ (Vector const _A, Vector const _B) { return Vector(_A.X + _B.X, _A.Y + _B.Y, _A.Z + _B.Z); }
-	friend Vector operator- (Vector const _A, Vector const _B) { return Vector(_A.X - _B.X, _A.Y - _B.Y, _A.Z - _B.Z); }
-	// скалярное произведение
-	friend num operator*(Vector const _a, Vector const _b) { return _a.X * _b.X + _a.Y * _b.Y + _a.Z * _b.Z; } 
-	// умножение на число
-	friend Vector operator*(num const _c, Vector const _V) { 
-		Vector V(_V);
-		Vector c_V = V.mult3(_c);
-		return c_V;
-	} 
-	//Векторное произведение
-	friend Vector operator^(Vector const _a, Vector const _b) {
-		Vector A(_a);
-		Vector normV = A.cross3(_b);		
-		return normV;
-	}
-};
-
-// Отрезок
-class Segment :IShape
-{
-private:
-	Point L_A;
-	Point L_B;
-public:
-
-	Point getA() const { return L_A; };
-	Point getB() const { return L_B; };
-
-	Vector Q;
-	Vector R;
-
-	// направляющий вектор
-	Vector dir;
-
-	explicit Segment(Point _A, Point _B) : L_A(_A), L_B(_B) {
-		dir = Vector(L_A, L_B);
-		Q = Vector(L_A);
-		R = Vector(L_B);
-	}
-
-	num len() { return dir.len(); }
-	num area() { return 0; }
-};
-
-
-//    треугольник
-class Triangle : IShape
-{
-private:
-	Point P_A;
-	Point P_B;
-	Point P_C;
-
-public:
-
-	Vector V_AB;
-	Vector V_AC;
-	Vector V_BC;
-	
-	explicit Triangle( Point _A, Point _B, Point _C) : P_A(_A), P_B(_B), P_C(_C)
-	{
-		V_AB = Vector(P_A, P_B);
-		V_AC = Vector(P_A, P_C);		
-		V_BC = Vector(P_B, P_C);
-	}
-	
-	Point getA() const { return P_A; };
-	Point getB() const { return P_B; };
-	Point getC() const { return P_C; };
-
-	// описать уравнение плоскости через 3 точки
-	// 
-	//  вектор нормали
-	Vector norm()
-	{
-		Vector V_N = V_AB ^ V_AC;
-		return V_N;
-	}
-
-	friend ostream& operator<<(ostream& out, Triangle& _T) {
-		out << _T.P_A << " " << _T.P_B << " " << _T.P_C << endl;
-		return out;
-	}
-
-	Triangle Triangle_Proj2D(int _O_XYZ)
-	{
-		Point _A, _B, _C;
-		Triangle tr(_A, _B, _C);
-		
-		if (_O_XYZ == 0)
-		{
-			_A = Point(P_A.Y, P_A.Z, 0);
-			_B = Point(P_B.Y, P_B.Z, 0);
-			_C = Point(P_C.Y, P_C.Z, 0);
-			tr = Triangle(_A, _B, _C);
-		}
-		else if (_O_XYZ == 1)
-		{
-			_A = Point(P_A.X, P_A.Z, 0);
-			_B = Point(P_B.X, P_B.Z, 0);
-			_C = Point(P_C.X, P_C.Z, 0);
-			tr = Triangle(_A, _B, _C);
-		}
-		else if (_O_XYZ == 2)
-		{
-			_A = Point(P_A.X, P_A.Y, 0);
-			_B = Point(P_B.X, P_B.Y, 0);
-			_C = Point(P_C.X, P_C.Y, 0);
-			tr = Triangle(_A, _B, _C);
-		}
-
-		return tr;
-	}
-
-	num len() { return V_AB.len() + V_AC.len() + V_BC.len(); }
-	num area() { return (V_AB ^ V_AC).len() / 2 ; }
-};
 
 ostream& operator<< (std::ostream& out, const Point& point)
 {
 	out << "Point(" << point.X << ", " << point.Y << ", " << point.Z << ')';
 	return out;
-}
+};
 
 istream& operator>> (istream& in, Point& point)
 {
-	in >> point.X >> point.Y >> point.Z ;
+	in >> point.X >> point.Y >> point.Z;
 	return in;
-}
+};
 
+//Moeller–Trumbore intersection algorithm
 bool RayIntersectsTriangle(Segment *segment,
 						   Triangle* inTriangle,
 						   Vector& outIntersectionPoint)
@@ -356,9 +149,9 @@ public:
 
 		area2 = (_B.X - _A.X) * (_C.Y - _A.Y) - (_C.X - _A.X) * (_B.Y - _A.Y);
 
-		if (area2 > 0.5) 
+		if (area2 >= 0.5) 
 			return 1;
-		else if (area2 < 0.5) 
+		else if (area2 <= -0.5) 
 			return -1;
 		
 		return 0;
@@ -381,13 +174,13 @@ public:
 		return 0;
 	}
 
-
+	// определяем на какую плоскость проецируем
 	int PlaneCoefficients(double* D)
 	{
 		int i;
 		double t;
 		double biggest = 0.0;
-		int m = 0;
+		int max_XYZ = 0;
 
 		Vector v_N = triangle->norm();
 
@@ -404,10 +197,10 @@ public:
 			if (t > biggest) 
 			{
 				biggest = t;
-				m = i;
+				max_XYZ = i;
 			}
 		}
-		return m;
+		return max_XYZ;
 	}
 	
 	//'V: p coincides with a Vertex of T. 
@@ -470,7 +263,7 @@ public:
 	{
 		Vector v_q  = segment->Q;
 		Vector v_r  = segment->R;
-		Vector v_rq = segment->dir;
+		Vector v_qr = segment->dir;
 		Vector v_N  = triangle->norm();
 
 		Vector v_P;
@@ -481,7 +274,7 @@ public:
 		*_m_XYZ = PlaneCoefficients(&D);
 
 		num = D - (v_q * v_N);
-		denom = (v_rq * v_N);
+		denom = (v_qr * v_N);
 
 		if (denom != 0)
 		{
@@ -492,19 +285,19 @@ public:
 			return (num == 0) ? IntersectionType::p : IntersectionType::p0;
 		}
 		 
-		v_P = v_q + t * v_rq;
+		v_P = v_q + t * v_qr;
 
 		iPoint = v_P.getPoint(); // пересечение с плоскостью
-
-		if (t > 0 && t < 1)
+		
+		if (t > EPSILON && t < 1.0-EPSILON) // t>0 && t<1
 			return IntersectionType::p1;
 
-		if (num == 0)
+		if (abs(num) <= EPSILON) // num == 0
 			return IntersectionType::q;
 
-		if (num == denom)
+		if (abs(num - denom) <= EPSILON ) // num == denom
 			return IntersectionType::r;
-
+				
 		return IntersectionType::p0;
 	}
 
@@ -515,6 +308,7 @@ public:
 		Vector v_A = triangle->getA(), 
 			   v_B = triangle->getB(), 
 			   v_C = triangle->getC();
+
 
 		vol0 = VolumeSign(segment->Q, v_A, v_B, segment->R);
 		vol1 = VolumeSign(segment->Q, v_B, v_C, segment->R);
@@ -557,11 +351,13 @@ public:
 
 		if (code == IntersectionType::q)
 		{
-			return IntersectionTriangle3D(segment->getA(), m_XYZ);
+			Point Q = segment->getA();
+			return IntersectionTriangle3D(Q, m_XYZ);
 		}
 		else if (code == IntersectionType::r)
 		{
-			return IntersectionTriangle3D(segment->getB(), m_XYZ);
+			Point R = segment->getB();
+			return IntersectionTriangle3D(R, m_XYZ);
 		}
 		else if (code == IntersectionType::p)
 		{
@@ -573,9 +369,7 @@ public:
 			return inType;
 		}			
 	}
-		
-	
-	// Линия пересекает треугольник ?
+				
 	static bool is_ray_cross_triangle(Segment* _segment, Triangle* _triangle)
 	{
 		Vector intersectionPoint;
@@ -586,29 +380,27 @@ public:
 
 		return true;
 	}
-
 };
 
+//Test 1, IntersectionType::F
+#define vA 2, 0, 1
+#define vB 0, 1, 0
+#define vC 0, 0, 3 
 
-// triangle
+#define fromA 0, 0, 0
+#define toB 0.6, 0.6, 0.6 
+
+//Test 2, IntersectionType::E
 #define vA 1, 0, 0
 #define vB 0, 1, 0
 #define vC 1, 1, 1 
 
-// segment q
-/*
-#define fromA 0, 0, 1
-#define toB 0, 0, 2 
-*/
-
-// segment r
 #define fromA 0, 0, 0
-#define toB 1, 1, 0.5 
+#define toB 0.5, 0.5, 0
+
 
 int main()
-{	
-	system("chcp 1251");
-
+{		
 	const Point pA(vA), pB(vB), pC(vC);	
 	const Point pntFrom(fromA), pntTo(toB);
 
@@ -623,10 +415,21 @@ int main()
 	else
 	{			
 		IntersectionType inResult = segmentTriangleIntersection->IntersectionCalculate();
+							
+		if (inResult == IntersectionType::F || inResult == IntersectionType::E || inResult == IntersectionType::V)
+		{
+			cout << "Result: triangle intersection." << endl;
+			cout << "Intersection " << *segmentTriangleIntersection->getIntersectionPoint() << endl << endl;
+		}
+		else
+		{
+			cout << "Result: no intersection " << endl;
+		}
+
+		cout << endl << "Description: " << txt_result[(int)inResult] << endl;
 		
 		SegmentTriangleIntersection::is_ray_cross_triangle(segment, triangle);
 
-		std::cout << "Intersection: " << *segmentTriangleIntersection->getIntersectionPoint() << std::endl;			
 	}	
 	delete segmentTriangleIntersection;
 
